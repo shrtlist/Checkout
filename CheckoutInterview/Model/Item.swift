fadb372e14a4bae3199a97abf31700b52ac990d2
Marco Abundo <shrtlist@gmail.com>
2024-06-26 09:32:36 -0700

    Working according to specs

---
+++ CheckoutInterview/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  CheckoutInterview
+//
+//  Created by Marco Abundo on 6/24/24.
+//
+
+import Foundation
+
+struct Item: Decodable {
+    let name: String
+    var displayPrice: String?
+}
---
+++ CheckoutInterview/Model/OrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  OrderResponse.swift
+//  CheckoutInterview
+//
+//  Created by Marco Abundo on 6/24/24.
+//
+
+import Foundation
+
+struct OrderResponse: Decodable {
+    let id: String
+    let items: [Item]
+}
